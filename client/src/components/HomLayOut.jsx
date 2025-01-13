06fda729f937842a3dcc4a5542b05ce8b5532775
stonoy <sowrabh2013@gmail.com>
2025-01-13 22:58:46 +0530

    ui just started

--- client/src/components/HomLayOut.jsx
+++ client/src/components/HomLayOut.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 const HomLayOut = () => {
-  console.log("home")
+  console.log("home.")
   return (
     <>
       <h1>Home</h1>
